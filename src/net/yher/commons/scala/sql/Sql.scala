3c956f74327021a9dd98481437ebc5b3433cfa4b
Hisanaga MAKINO <hisaboh@yher.net>
2009-04-30 18:11:13 +0900

    newInstanceメソッドを追加。

--- src/net/yher/commons/scala/sql/Sql.scala
+++ src/net/yher/commons/scala/sql/Sql.scala
@@ -1,7 +1,23 @@
 package net.yher.commons.scala.sql
 
-class Sql {
-  def eachRow(sql:String) {
+import java.sql._
+
+class Sql(val con : Connection) {
+  def eachRow(sql:String)(block:ResultSet => Unit) {
+    var ps : PreparedStatement = null
+    try {
+      ps = con.prepareStatement(sql)
+      val rs = ps.executeQuery()
+      block(rs)
+    } finally {
+      if (ps != null) ps.close()
+    }
     sql
+  }
+}
+
+object Sql {
+  def newInstance(url : String) : Sql = {
+    return new Sql(DriverManager.getConnection(url));
   }
 }
