c14fad86192d017a864aa0e9457086cb41dc2374
Hisanaga MAKINO <hisaboh@yher.net>
2009-05-01 16:05:42 +0900

    整形のみ。

--- src/net/yher/commons/scala/sql/Sql.scala
+++ src/net/yher/commons/scala/sql/Sql.scala
@@ -3,29 +3,28 @@ package net.yher.commons.scala.sql
 import java.sql._
 
 class Sql(val con : Connection) {
-  def eachRow(sql:String)(block:ResultSet => Unit) {
-    var ps : PreparedStatement = null
-    var rs : ResultSet = null
-    try {
-      ps = con.prepareStatement(sql)
-      rs = ps.executeQuery()
-      while (rs.next()) {
-        block(rs)
-      }
-    } finally {
-      if (rs != null) rs.close()
-      if (ps != null) ps.close()
+    def eachRow(sql:String)(block:ResultSet => Unit) {
+        var ps : PreparedStatement = null
+        var rs : ResultSet = null
+        try {
+            ps = con.prepareStatement(sql)
+            rs = ps.executeQuery()
+            while (rs.next()) {
+                block(rs)
+            }
+        } finally {
+            if (rs != null) rs.close()
+            if (ps != null) ps.close()
+        }
     }
-    sql
-  }
 }
 
 object Sql {
-  def newInstance(url : String) : Sql = {
-    return new Sql(DriverManager.getConnection(url));
-  }
-  
-  def newInstance(url : String, user : String, password : String) {
-    return new Sql(DriverManager.getConnection(url, user, password))
-  }
+    def newInstance(url : String) : Sql = {
+        return new Sql(DriverManager.getConnection(url));
+    }
+
+    def newInstance(url : String, user : String, password : String) {
+        return new Sql(DriverManager.getConnection(url, user, password))
+    }
 }
